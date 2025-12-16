d465ebd68b5a4c6cf568c2119ab479f06c084132
Hemant Joshi <184886086+Hemant-3922@users.noreply.github.com>
2025-12-16 11:24:12 +0530

    leetcode part added in comment

--- day15/A_Chewba_and_Number.java
+++ day15/A_Chewba_and_Number.java
@@ -24,3 +24,46 @@ public class A_Chewba_and_Number {
         System.out.println(ans.toString());
     }
 }
+
+//leetcode
+/*class Solution {
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+                fw.write("0");
+            } catch (Exception e) {
+            }
+        }));
+    }
+
+    public String reverseVowels(String s) {
+        // convert to char array
+        char[] arr = s.toCharArray();
+
+        int L = 0, R = arr.length - 1;
+
+        while (L < R) {
+            while (L < R && !isVowel(arr[L])) {
+                L++;
+            }
+            while (L < R && !isVowel(arr[R])) {
+                R--;
+            }
+
+            // swap vowels
+            char temp = arr[L];
+            arr[L] = arr[R];
+            arr[R] = temp;
+
+            L++;
+            R--;
+        }
+
+        return new String(arr);
+    }
+
+    private boolean isVowel(char c) {
+        // TODO: complete this small function
+        return "aeiouAEIOU".indexOf(c) != -1;
+    }
+} */
